cd397e38d136c6375a3cdf8d37593ae6df97ddc5
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-04 19:58:39 +0530

    Update testSym.py

--- testSym.py
+++ testSym.py
@@ -7,7 +7,7 @@ def testSym():
             sym = Sym(0, colName)
             for value in data:
                 sym.add(value)
-    mode = sym.mid()
-    entropy = sym.div()
-    entropy = (1000*entropy) // 1/1000
-    return mode == "a" and 1.37 <= entropy <= 1.38
+            mode = sym.mid()
+            entropy = sym.div()
+            entropy = (1000*entropy) // 1/1000
+            return mode == "a" and 1.37 <= entropy <= 1.38
