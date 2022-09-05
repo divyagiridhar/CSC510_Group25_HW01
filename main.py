f3e06a5371ec1cf28374e8cf048eaf7da7a17484
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-06 05:09:27 +0530

    Create main.py
    Reading csv file and organizing columns. Using these columns to distinguish between Sym and Num classes.

---
+++ main.py
@@ -0,0 +1,45 @@
+import csv
+from curses.ascii import isupper
+from Sym import Sym
+from num import Num
+
+filename = open('source.csv', 'r')
+file = csv.DictReader(filename)
+
+Clndrs = []
+Volume = []
+Hp = []
+Lbs = []
+Acc = []
+Model = []
+origin = []
+Mpg = []
+
+for col in file:
+    Clndrs.append(col["Clndrs"])
+    Volume.append(col["Volume"])
+    Hp.append(col["Hp:"])
+    Lbs.append(col["Lbs-"])
+    Acc.append(col["Acc+"])
+    Model.append(col["Model"])
+    origin.append(col["origin"])
+    Mpg.append(col["Mpg+"])
+
+df = {"Clndrs": Clndrs, "Volume": Volume, "Hp:": Hp, "Lbs-": Lbs, "Acc+": Acc, "Model": Model, "origin": origin, "Mpg+": Mpg}
+
+colPosition = 0
+for colName, data in df.items():
+    if colName[0].islower():
+        # print('This col is sym')
+        sym = Sym(colPosition, colName)
+        for value in data:
+            sym.add(value)
+        colPosition+=1
+    elif colName[0].isupper():
+        # print('This col is Num')
+        num = Num(colPosition, colName)
+        for value in data:
+            num.add(value)
+        colPosition+=1
+
+
