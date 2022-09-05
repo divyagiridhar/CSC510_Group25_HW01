7e666dbc205594daba80171deec23ab7764b97d5
Shreyas Titus <75424941+ShreyasTitus@users.noreply.github.com>
2022-09-05 13:46:44 -0400

    Update and rename testBigNum to testBigNum.py

---
+++ testBigNum.py
@@ -0,0 +1,12 @@
+import unittest
+from finalSym import Sym
+
+class testReturnValues(unittest.TestCase):
+   
+    def testBigNum(self):
+        num = num()
+        the.nums = 32
+        for i in range(1,1000):
+            num.add(i)
+        oo(num.nums())
+        self.assertEqual(32, len(num.__has))
