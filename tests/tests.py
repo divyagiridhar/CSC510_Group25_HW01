1a08b796dc7ac66a25ed3e93b3e7f101657e577b
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-08 21:31:21 +0530

    Create tests.py

---
+++ tests/tests.py
@@ -0,0 +1,48 @@
+import imp
+import unittest
+from num import Num
+from Sym import Sym
+from config import t
+
+class testReturnValues(unittest.TestCase):
+    
+    def testNum(self):
+        t["nums"] = 1000
+        numData = {"Cldrs":list(range(1,100+1))}
+        for colName, data in numData.items():
+            if colName[0].isupper():
+                num = Num(0, colName)
+                for value in data:
+                    num.add(value)
+                mode = num.mid()
+                div = num.div()
+                num.print_Num(div, mode)
+                self.assertTrue(50 <= mode <= 52) and self.assertTrue(30.5 <= div <= 32)
+
+    def testSym(self):
+        symFreq = {"origin":["a", "a", "a", "a", "b", "b", "c"]}
+        for colName, data in symFreq.items():
+            if colName[0].islower():
+                # This column is a symbol
+                sym = Sym(0, colName)
+                for value in data:
+                    sym.add(value)
+                mode = sym.mid()
+                entropy = sym.div()
+                entropy = (1000*entropy) // 1/1000
+                sym.print_Sym(entropy, mode)
+                self.assertEqual(mode, "a") and self.assertTrue(1.37 <= entropy <= 1.38)
+
+    def testBigNum(self):
+        t["nums"] = 32
+        numData = {"Cldrs":list(range(1,1000+1))}
+        for colName, data in numData.items():
+            if colName[0].isupper():
+                num = Num(0, colName)
+                for value in data:
+                    num.add(value)
+            num.print_BigNum()
+        self.assertEqual(32, len(num._has))
+
+if __name__  == '__main__':
+    unittest.main()
