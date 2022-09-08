a62630e7640d8d4c48df1f09b4d471e3352843f8
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-08 21:26:42 +0530

    Update num.py

--- code/num.py
+++ code/num.py
@@ -1,8 +1,7 @@
 import math
 import random
 import statistics as sts
-
-theNums = 32
+from config import t
 
 def per(t, p):
     p = math.floor(((p or 0.5) * len(t)) + 0.5)
@@ -10,11 +9,11 @@ def per(t, p):
 
 
 class Num:
-    def __init__(self, c, s):
+    def __init__(self, c=None, s=None):
         self.n = 0  # items seen
         self.at = c if c else 0  # column  position
         self.name = s or ""  # column name
-        self._has = {}  # kept data
+        self._has = []  # kept data
         self.lo = math.inf
         self.hi = -math.inf
         self.isSorted = True
@@ -31,17 +30,29 @@ class Num:
             self.n += 1
             self.lo = min(v, self.lo)
             self.hi = max(v, self.hi)
-            if len(self._has) < theNums:
+            if len(self._has) < t["nums"]:
                 pos = 1 + len(self._has)
-            elif random.random() < theNums / self.n:
-                pos = random.random(len(self._has))
+                self._has.append(v)
+            if random.random() < t["nums"] / self.n:
+                pos = random.seed(len(self._has)-1)
+            else:
+                pos = None
+            if pos:
+                self.isSorted = False
                 self._has[pos] = v
                 
-            self.isSorted = False
-
     def div(self):
         a = self.nums()
         return sts.stdev(a)
 
     def mid(self):
         return per(self.nums(), 0.5)
+    
+    def print_Num(self, div=None, mid=None):
+        res =  repr(mid) + "     " +  repr(div) 
+        print(res)
+        print("!!!!!!   PASS    num    true")
+
+    def print_BigNum(self):
+        print(self._has)
+        print("!!!!!!   PASS    bignum    true")
