4a9c8cfa613161d5e561e9242b81d93dda264208
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-17 04:17:30 +0530

    Update and rename cols.py to Cols.py

--- code/Cols.py
+++ code/Cols.py
@@ -1,7 +1,5 @@
-# from main import for_sym
-# from main import for_num
-from num import Num
 from Sym import Sym
+from Num import Num
 
 def push(t, x):
     t[1 + len(t)] = x
@@ -9,28 +7,25 @@ def push(t, x):
 
 class Cols:
     def __init__(self, names):
-        self.names = names
-        self.all = {}
-        self.klass = None
-        self.x = {}
-        self.y = {}
+        self.names=names  
+        self.all=[]       
+        self.klass=None   
+        self.x=[]         
+        self.y=[]         
         
         for c, s in names.items():
             if s.isupper():
                 Num(c, s)
                 col = push(self.all, (c, s))
-            
             if s.islower():
                 Sym(c, s)
                 col = push(self.all, (c, s))
             
             if not s.find(':'):
                 if s.find('+' or '-'):
-                    self.y.append(s)
-                    push(s, col)
+                    self.y.append(col) 
                 else:
-                    self.x.append(s)
-                    push(s, col)
-                    
+                    self.x.append(col)
+                
             if s.find('!'):
                 self.klass = col
