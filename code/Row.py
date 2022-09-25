3ba19e57cfaca71c0f9ee3f22bfe32240e60f346
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-24 22:10:53 -0400

    Update Row.py

--- code/Row.py
+++ code/Row.py
@@ -1,17 +1,9 @@
-from Utils import copy
+import Utils
 
-def copy(t):
-    if type(t) != dict:
-        return t
-    u = {}
-    for k, v in t.items():
-        u[k] = copy(v)
-    return u
-    
 class Row:
     def __init__(self, t):
         self.cells = t
-        self.cooked = copy(t)
+        self.cooked = Utils.copy(t)
         self.isEvaled = False
-        
-        return self.cells, self.cooked, self.isEvaled
+
+
