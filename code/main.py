eee37a7006fe18f88966f964ca041d087f5f8ad1
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-09-24 22:11:35 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,46 +1,74 @@
-import csv
-from curses.ascii import isupper
-from Sym import Sym
-from num import Num
+import sys
+import Utils
+import Sym 
+import Num
+import Data
+sys.path.insert(0, './code')
 
-filename = open('source.csv', 'r')
-file = csv.DictReader(filename)
+def test_the():
+    Utils.oo(Utils.the)
+    return True
 
-Clndrs = []
-Volume = []
-Hp = []
-Lbs = []
-Acc = []
-Model = []
-origin = []
-Mpg = []
+def test_sym():
+    sym = Sym.Sym()
+    for x in ["a", "a", "a", "a", "b", "b", "c"]:
+        sym.add(x)
+    mode, entropy = sym.mid(), sym.div()
+    entropy = (1000 * entropy) // 1 / 1000
+    Utils.oo({"mid": mode, "div": entropy})
+    return mode == "a" and 1.37 <= entropy and entropy <= 1.38
 
-for col in file:
-    Clndrs.append(col["Clndrs"])
-    Volume.append(col["Volume"])
-    Hp.append(col["Hp:"])
-    Lbs.append(col["Lbs-"])
-    Acc.append(col["Acc+"])
-    Model.append(col["Model"])
-    origin.append(col["origin"])
-    Mpg.append(col["Mpg+"])
+def test_num():
+    num = Num.Num()
+    for i in range(1, 100):
+        num.add(i)
+    mid, div = num.mid(), num.div()
+    print(mid, div)
+    return 50 <= mid and mid <= 52 and 30.5 < div and div < 32
 
-df = {"Clndrs": Clndrs, "Volume": Volume, "Hp:": Hp, "Lbs-": Lbs, "Acc+": Acc, "Model": Model, "origin": origin, "Mpg+": Mpg}
+def test_bignum():
+    num = Num.Num()
+    Utils.the['nums'] = 32
+    for i in range(1, 1000):
+        num.add(i)
+    res = dict(sorted(num.nums().items(), key=lambda item: item[1]))
+    print(res.values())
+    return 32 == len(num._has)
 
-colPosition = 0
-for colName, data in df.items():
-    if colName[0].islower():
-        sym = Sym(colPosition, colName)
-        for value in data:
-            sym.add(value)
-        div = sym.div()
-        mid = sym.mid()
-        sym.print_Sym(div, mid)
-    if colName[0].isupper():
-        num = Num(colPosition, colName)
-        for value in data:
-            num.add(value)
-        div = num.div()
-        mid = num.mid()
-        num.print_Num(div, mid)
-    colPosition+=1
+def test_data():
+    d = Data.Data("../data/data.csv")
+    for y in d.cols.y:
+        Utils.oo(y)
+    return True
+
+def test_stats():
+    data = Data.Data("../data/data.csv")
+    def div(col):
+        return col.div()
+    
+    def mid(col):
+        return col.mid()
+    print('xmid=', Utils.o(data.stats(2, data.cols.x, mid)))
+    print('xdiv=', Utils.o(data.stats(3, data.cols.x, div)))
+    print('ymid=', Utils.o(data.stats(2, data.cols.y, mid)))
+    print('ymid=', Utils.o(data.stats(3, data.cols.y, div)))
+    return True
+
+def test_csv():
+    global n 
+    n=0
+    
+    def function_row(r):
+        global n 
+        n+=1
+        return n if n>10 else Utils.oo(r)
+    Utils.parse_csv("../data/data.csv", function_row)
+    return True
+
+test_the()
+test_sym()
+test_num()
+test_bignum()
+test_data()
+test_stats()
+test_csv()
